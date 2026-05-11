393f10da2da97ec249c13070ba05a6e603dc4329
alevir115 <alevir115aeropuerto@gmail.com>
2026-05-11 09:51:11 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+    URL: 'https://uabddcmlpmomffdrpavg.supabase.co';
+    KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhYmRkY21scG1vbWZmZHJwYXZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NzA1ODQsImV4cCI6MjA5MzQ0NjU4NH0.bnLf5TdKW8sOIadm4LU0SZlMbQgQOzp_q3l03KLnVbw';
+
+};
