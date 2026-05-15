27b989d857d6bed1a24504e869a74368583463f7
alevir115 <alevir115aeropuerto@gmail.com>
2026-05-15 09:59:05 -0300

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    URL: 'https://vytkrwntfmxnnjedsfid.supabase.co';
+    URL: 'https://uabddcmlpmomffdrpavg.supabase.co';
     KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dGtyd250Zm14bm5qZWRzZmlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNzU5NzEsImV4cCI6MjA5Mjg1MTk3MX0.mH7H0ZzoYHArewRZEgLRWp3Q_8ubAHMgA7VkekT5Kps';
 
 };
